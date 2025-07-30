447d2364b169a7fca52c27aded3c8c39c86f9649
IsraeliGuitar <rabtests@gmail.com>
2025-07-30 20:13:24 +0000

    feat: Add ad-iframe-width setting for advertisement configuration

--- backend/settings.go
+++ backend/settings.go
@@ -18,6 +18,7 @@ type ReplaceRegex struct {
 
 type SettingConfig struct {
 	AdSrc                   string
+	AdWidth                 int64
 	RequireAuth             bool
 	RequireAuthForViewFiles bool
 	RegexReplace            []*ReplaceRegex
@@ -75,6 +76,9 @@ func (s *Settings) ToConfig() *SettingConfig {
 		switch setting.Key {
 		case "ad-iframe-src":
 			config.AdSrc = setting.GetString()
+
+		case "ad-iframe-width":
+			config.AdWidth = setting.GetInt()
 
 		case "require_auth":
 			config.RequireAuth = setting.GetBool()
