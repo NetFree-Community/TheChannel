13323276862f55859216c48641f9fadafff5ccb7
IsraeliGuitar <rabtests@gmail.com>
2025-07-30 14:15:27 +0300

    some cleanup

--- frontend/src/app/components/channel/channel-header/channel-header.component.ts
+++ frontend/src/app/components/channel/channel-header/channel-header.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@a
 import { NgIf } from "@angular/common";
 import {
   NbButtonModule,
-  NbContextMenuModule,
+  NbContextMenuModule, NbDialogService,
   NbIconModule,
   NbMenuItem,
   NbMenuService,
@@ -16,7 +16,7 @@ import { Title } from '@angular/platform-browser';
 import { AuthService, User } from '../../../services/auth.service';
 import { ChatService } from '../../../services/chat.service';
 import { NotificationsService } from '../../../services/notifications.service';
-import { AdminService } from "../../../services/admin.service";
+import { AdminPanelComponent } from "../../admin/admin-panel.component";
 
 @Component({
   selector: 'app-channel-header',
@@ -46,9 +46,11 @@ export class ChannelHeaderComponent implements OnInit {
       }
     ];
   }
+
   get userInfo() {
     return this._userInfo;
   }
+
   private _userInfo?: User;
 
   @Output()
@@ -66,7 +68,7 @@ export class ChannelHeaderComponent implements OnInit {
     private router: Router,
     public notificationsService: NotificationsService,
     private titleService: Title,
-    private adminService: AdminService
+    private dialogService: NbDialogService,
   ) {
   }
 
@@ -87,7 +89,7 @@ export class ChannelHeaderComponent implements OnInit {
             this.logout();
             break;
           case 'people-outline':
-            this.adminService.openAdminPanel();
+            this.dialogService.open(AdminPanelComponent, { closeOnBackdropClick: true });
             break;
         }
       });
--- frontend/src/app/services/admin.service.ts
+++ frontend/src/app/services/admin.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { ChatFile, ChatMessage } from './chat.service';
 import { ResponseResult } from '../models/response-result.model';
-import Viewer from "viewerjs";
-import { AdminPanelComponent } from "../components/admin/admin-panel.component";
-import { NbDialogService } from "@nebular/theme";
 
 export interface PrivilegeUser {
   id?: string;
@@ -27,7 +24,6 @@ export class AdminService {
 
   constructor(
     private http: HttpClient,
-    private dialogService: NbDialogService
   ) { }
 
   async getUsersAmount(): Promise<number> {
@@ -77,9 +73,5 @@ export class AdminService {
 
   setSettings(settings: Setting[]): Promise<ResponseResult> {
     return firstValueFrom(this.http.post<ResponseResult>('/api/admin/settings/set', settings));
-  }
-
-  openAdminPanel() {
-    this.dialogService.open(AdminPanelComponent, { closeOnBackdropClick: true});
   }
 }
